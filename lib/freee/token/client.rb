205f17adfc6a618966e47447696fd51259facf88
八木 辰弥 <t.yagi@Mac-46.local>
2018-07-10 11:39:43 +0900

    認証コードの取得テストの実装

--- lib/freee/token/client.rb
+++ lib/freee/token/client.rb
@@ -1,21 +1,38 @@
 require 'oauth2'
+require 'faraday'
+
 module Freee
   module Api
     class Token
       def initialize(app_id, secret)
-        @client = OAuth2::Client.new(
-          app_id,
-          secret,
-          site: "https://secure.freee.co.jp/"
-        )
+        options = {
+          site: "https://api.freee.co.jp/",
+          authorize_url: 'https://secure.freee.co.jp/oauth/authorize',
+          token_url: '/oauth/token'
+        }
+        @client = OAuth2::Client.new(app_id, secret, options) do |conn|
+          conn.request :url_encoded
+          conn.request :json
+          conn.response :json, content_type: /\bjson$/
+          conn.adapter Faraday.default_adapter
+        end
       end
 
-      def development_authorize
-        @client.auth_code.authorize_url(redirect_uri: "urn:ietf:wg:oauth:2.0:oob")
+      def development_authorize(app_id)
+        client = Faraday.new(url: "https://secure.freee.co.jp")
+        res = client.get do |req|
+          req.url "/oauth/authorize",
+          req.params = {
+            client_id: app_id,
+            redirect_uri: "urn:ietf:wg:oauth:2.0:oob",
+            response_type: "code"
+          }
+        end
+        res.body
       end
-
+    
       def get_access_token(code)
-        token = @client.auth_code.authorize_url(code, redirect_uri: "https://api.freee.co.jp/oauth/token")
+        token = @client.auth_code.get_token(code, redirect_uri: "https://api.freee.co.jp/oauth/token")
       end
     end
   end

ea90fb339d4702b9c5588acb323849a4e28e3ff7
himrock922 <himrock922@gmail.com>
2018-07-29 00:37:54 +0900

    Specテストの追加

--- spec/freee/token_spec.rb
+++ spec/freee/token_spec.rb
@@ -10,5 +10,20 @@ RSpec.describe Freee::Api::Token do
       expect(subject.client.id).to eq('dummy_client_id')
       expect(subject.client.secret).to eq('dummy_client_secret')
     end
+    it 'assigns site from the options hash' do
+      expect(subject.client.site).to eq('https://api.freee.co.jp')
+    end
+    it 'assigns authorize_uri from the options hash' do
+      expect(subject.client.site).to eq('https://secure.freee.co.jp/oauth/authorize')
+    end
+    it 'assigns token_uri from the options hash' do
+      expect(subject.client.site).to eq('https://api.freee.co.jp/oauth/token')
+    end
+  end
+
+  describe '#development_authorize' do
+    it 'assigns development url∂' do
+      expect(subject).to eq('https://secure.freee.co.jp/oauth/authorize?client_id=dummy_token7&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code')
+    end
   end
 end
